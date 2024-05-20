04ad6d4955513a4e13647b9d8aa2db307488d993
marco hernandez <mh94.design@gmail.com>
2024-05-20 11:38:28 -0600

    3.3: Phonebook backend step 3

--- index.js
+++ index.js
@@ -35,6 +35,18 @@ app.get('/info', (request, response) => {
   response.send(`Phonebook has info for ${personsNum} people <br /><br /> ${date}`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = persons.find(note => note.id === id)
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.statusMessage = "Person not found"
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
